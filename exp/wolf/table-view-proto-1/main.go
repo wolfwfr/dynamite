25c111c162715a5df4f644d6fa2a6a1a38eb2537
Wolf <wolffreije@innius.com>
2026-04-17 23:15:57 +0200

    feat [table-view-proto]: bordered window sections

---
+++ exp/wolf/table-view-proto-1/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
+)
+
+type model struct {
+	window struct {
+		width  int
+		height int
+	}
+	listPane    tea.Model
+	detailsPane tea.Model
+}
+
+var (
+	borderStyle = lipgloss.NewStyle().
+		Align(lipgloss.Center, lipgloss.Center).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("#2381CF"))
+)
+
+func (m model) applySize() {
+	borderStyle = borderStyle.
+		Height(m.window.height - 2).
+		Width(m.window.width / 2)
+}
+
+func newModel() model {
+	m := model{}
+	return m
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		m.window.height = msg.Height
+		m.window.width = msg.Width
+	}
+
+	m.applySize()
+
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	s := strings.Builder{}
+	s.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, borderStyle.Render("This is the future list pane"), borderStyle.Render("This is the future details-pane")))
+	v := tea.NewView(s.String())
+	v.AltScreen = true // fullscreen
+	return v
+}
+
+func main() {
+	p := tea.NewProgram(newModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
